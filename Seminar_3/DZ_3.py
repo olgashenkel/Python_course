1286e605c066ac11d4e900a2f57ee299d535048a
olga_shenkel <olga_shenkel@mail.ru>
2024-04-27 01:10:29 +0200

    add Python_course

--- Seminar_3/DZ_3.py
+++ Seminar_3/DZ_3.py
@@ -29,19 +29,39 @@ k = 'ноутбук'
 # 12
 '''
 
-alphabet = {1:'AEIOULNSTR',
-      	2:'DG',
-      	3:'BCMP',
-      	4:'FHVWY',
-      	5:'K',
-      	8:'JZ',
-      	10:'QZ',
-		1:'АВЕИНОРСТ',
-      	2:'ДКЛМПУ',
-      	3:'БГЁЬЯ',
-      	4:'ЙЫ',
-      	5:'ЖЗХЦЧ',
-      	8:'ШЭЮ',
-      	10:'ФЩЪ'}
-word = input('Введите слово: ').upper()
-print(sum([k for i in word for k, v in alphabet.items() if i in v]))
+k = 'lizard'
+summa = 0
+
+alphabet = {1:'AEIOULNSTRАВЕИНОРСТ',
+      	2:'DGДКЛМПУ',
+      	3:'BCMPБГЁЬЯ',
+      	4:'FHVWYЙЫ',
+      	5:'KЖЗХЦЧ',
+      	8:'JXШЭЮ',
+      	10:'QZФЩЪ'}
+
+for letter in k:
+	for key, value in alphabet.items():
+		if letter.upper() in value:
+			summa += key
+print(summa)
+
+
+"""
+Решение с сайта:
+
+points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
+points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+word = k.upper()  # переводим все буквы в верхний регистр
+count = 0
+for i in word:
+    if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+        for j in points_en:
+            if i in points_en[j]:
+                count = count + j
+    else:
+        for j in points_ru:
+            if i in points_ru[j]:
+                count = count + j
+print(count)
+"""
