292850a7a9ca36beb7cfdae2e4d645a2c65ff8c2
olga_shenkel <olga_shenkel@mail.ru>
2024-06-07 13:17:08 +0200

    add Python_course

--- Seminar_7/DZ_1.py
+++ Seminar_7/DZ_1.py
@@ -28,13 +28,40 @@
 ###############################################
 
 
+# def print_operation_table(operation, num_rows=9, num_columns=9):
+#     for row in range(1, num_rows + 1):
+#         if num_rows < 2:
+#             return print("ОШИБКА! Размерности таблицы должны быть больше 2!")
+#         for column in range(1, num_columns + 1):
+#             print(operation(row, column), end=' ')
+#         print()
+#
+#
+# print_operation_table(lambda x, y: x * y)
+
 def print_operation_table(operation, num_rows=9, num_columns=9):
-    for row in range(1, num_rows + 1):
-        if num_rows < 2:
-            return print("ОШИБКА! Размерности таблицы должны быть больше 2!")
-        for column in range(1, num_columns + 1):
-            print(operation(row, column), end=' ')
-        print()
+    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+    if num_rows < 2:
+        return print("ОШИБКА! Размерности таблицы должны быть больше 2!")
+    for i in a:
+        print(*[f"{x}" for x in i])
 
 
-print_operation_table(lambda x, y: x * y)
+print_operation_table(lambda x, y: x / y, 4, 4)
+
+
+# Решение с сайта:
+
+# def print_operation_table(operation, num_rows=9, num_columns=9):
+#     result = []
+#     if num_rows < 2 or num_columns < 2:
+#         print('ОШИБКА! Размерности таблицы должны быть больше 2!')
+#     else:
+#         for i in range(1, num_rows + 1):
+#             for j in range(1, num_columns + 1):
+#                 if j != num_columns :
+#                     result.append(f'{operation(i, j)} ')
+#                 else:
+#                     result.append(operation(i, j))
+#             result.append('\n')
+#         print(''.join([str(i) for i in result[:len(result) - 1]]))
