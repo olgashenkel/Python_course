3832b4633cffae8c60da9ddee24557ffa877ebb5
olga_shenkel <olga_shenkel@mail.ru>
2024-06-15 01:53:33 +0200

    add Python_course/Seminar_10

---
+++ Seminar_10/DZ_1.py
@@ -0,0 +1,35 @@
+# Урок 10. Построение графиков
+#############################
+# Задача 44:
+# В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца.
+# Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без get_dummies?
+######################
+# import random
+# lst = ['robot'] * 10
+# lst += ['human'] * 10
+# random.shuffle(lst)
+# data = pd.DataFrame({'whoAmI':lst})
+# data.head()
+########################
+
+import random
+
+import pandas as pd
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+# 1-ый способ:
+data['robot'] = [1 if lst_1 == 'robot' else 0 for lst_1 in data['whoAmI']]
+data['human'] = [1 if lst_2 == 'human' else 0 for lst_2 in data['whoAmI']]
+
+print(f'\n1-ый способ:\n {data.head(n=5)}\n')
+
+
+# 2-ой способ:
+data['robot'] = (data['whoAmI'] == 'robot').astype(int)
+data['human'] = (data['whoAmI'] == 'human').astype(int)
+
+print(f'2-ой способ:\n {data.head(n=5)}')
---
+++ Seminar_10/task_3.py
@@ -0,0 +1,39 @@
+# Задача №67.
+# 1. Создать новый столбец в таблице с пингвинами, который будет отвечать за
+# показатель длины клюва пингвина.
+# high - длинный(от 42)
+# middle - средний(от 35 до 42)
+# low - маленький(до 35)
+####################
+
+# Чтобы подключить датасет с пингвинами, воспользуйтесь данным скриптом:
+# penguins = sns.load_dataset("penguins")
+# penguins.head()
+
+#######################################################
+
+
+from pandas import read_csv
+from seaborn import histplot
+from matplotlib.pyplot import show
+
+penguins = read_csv('penguins.csv')
+
+penguins.loc[penguins['bill_length_mm'] < 35, 'height_group'] = 'low'
+
+penguins.loc[(penguins['bill_length_mm'] > 35) & (penguins['bill_length_mm'] < 42), 'height_group'] = 'middle'
+
+penguins.loc[penguins['bill_length_mm'] > 42, 'height_group'] = 'high'
+
+penguins.to_csv('penguins.csv', index=False) # поменять данные внутри самого файла
+
+
+############################################
+# Задача №69.
+# 1. Изобразить гистограмму по flipper_length_mm
+# с оттенком height_group. Сделать анализ
+
+############################################
+
+histplot(penguins, x='flipper_length_mm', hue='height_group')
+show()
