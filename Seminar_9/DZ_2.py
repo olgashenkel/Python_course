c4c8acd1fcd0507c76544fb359a3f0f9b8b67e78
olga_shenkel <olga_shenkel@mail.ru>
2024-06-14 06:21:59 +0200

    add Python_course/Seminar_9

---
+++ Seminar_9/DZ_2.py
@@ -0,0 +1,29 @@
+# Максимальная households
+
+# Дан файл california_housing_train.csv.
+# Найти максимальное значение переменной "households" в зоне минимального значения
+# переменной min_population и сохраните это значение в переменную max_households_in_min_population.
+# Используйте модуль pandas.
+###############################################
+
+from pandas import read_csv
+
+data = read_csv('california_housing_train.csv')
+
+# avg = data[data['population'] == 9]
+
+max_households_in_min_population = data[data['min_population'] == data['min_population'].min()]['households'].max()
+
+print(max_households_in_min_population)
+
+
+
+# Решение с сайта:
+# import pandas as pd
+#
+# df = pd.read_csv('california_housing_train.csv')
+# # Найти минимальное значение 'population'
+# min_population = df['population'].min()
+#
+# # Отфильтровать строки с минимальным значением 'population' и найти максимальное значение 'households'
+# max_households_in_min_population = df[df['population'] == min_population]['households'].max()
